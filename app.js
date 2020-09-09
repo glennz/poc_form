20e795d7355d2892391320af375a2a0a6e928b26
Glenn <glennzh@gmail.com>
2020-09-09 20:13:48 +1000

    update app.js

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
 class PocForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        postbackUrl: ""
-      };
-  
-      this.handleChange = this.handleChange.bind(this);
-    }
-  
-    handleChange(e) {
-      console.log("e", e.target.value);
-      this.setState({ postbackUrl: e.target.value });
-    }
-  
-    render() {
-      return (
-        <div>
-          <div>
-            <label>form post</label>
-            <input type="text" onChange={this.handleChange} />
-          </div>
-          <form action={this.state.postbackUrl} method="post">
-            <div>
-              <label>Account name</label>
-              <input type="text" name="name" />
-            </div>
-            <div>
-              <label>Credit Card number</label>
-              <input type="text" name="cardnumber" />
-            </div>
-            <div>
-              <button>Submit</button>
-            </div>
-          </form>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      postbackUrl: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
-  
+
+  handleChange(e) {
+    this.setState({ postbackUrl: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <label>form post </label>
+          <input type="text" onChange={this.handleChange} /> E.g.:
+          https://www.google.com
+          <br />
+          <br />
+        </div>
+        <form action={this.state.postbackUrl} method="post">
+          <div>
+            <label>Account name </label>
+            <input type="text" name="name" />
+            <br />
+            <br />
+          </div>
+          <div>
+            <label>Credit Card number </label>
+            <input type="text" name="cardnumber" />
+            <br />
+            <br />
+          </div>
+          <div>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
